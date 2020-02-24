81e0549d4ecfd08d96917c4e8cb98d878a88d455
Jonathan Tan <you@example.com>
2020-02-24 09:25:39 -0500

    adding changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import sum from './sum';
 import axios from 'axios';
 
+let messages = 
+
+{
+  "Scoops ahoy": "Scoops ahoy!", 
+  "Where are you located?": "Starcourt mall, Hawkins Indiana", 
+  "What do you think about kids?": "Man, kids are the worst! Who needs 'em, anyway?"
+}
+
 
 console.log(sum(1,2));
 
@@ -12,3 +20,69 @@ const main = async () => {
 main();
 
 
+function renderChat() { 
+  let chatContainer = document.getElementById("chat-container")
+  
+  let chatBox = document.createElement("div")
+  let formContainer = document.createElement("form")
+  formContainer.id = "form-container"
+  let header = document.createElement("h2")
+  let message = document.createElement("label")
+  let messageArea = document.createElement("textarea")
+  let sendButton = document.createElement("button")
+
+  
+
+  header.innerText = "Chat"
+  message.innerText = "Message"
+  messageArea.name = "name"
+  sendButton.innerText = "Send"
+
+  formContainer.addEventListener("submit", chatHandler)
+
+
+  
+  formContainer.append(header,message,messageArea, sendButton)
+  chatBox.append(formContainer)
+  chatContainer.append(chatBox)
+
+
+}
+
+
+function botResponse(question) { 
+  
+
+
+
+
+}
+
+
+/// handlers 
+
+function chatHandler(e) {
+  e.preventDefault()
+
+
+  console.log(e.target[0].value)
+
+  let chatScreen = document.getElementById("chat-screen")
+  let newChatBubble = document.createElement("p")
+  newChatBubble.innerText = e.target[0].value
+
+  chatScreen.appendChild(newChatBubble)
+
+  
+  // e.preventDefault()
+  // console.log(e)
+
+
+}
+
+
+
+
+
+
+renderChat()
